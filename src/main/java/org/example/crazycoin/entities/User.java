5c93aa23491083d79987ee55af9cb3c8cb20d038
José Manuel Galindo González <213505@ids.upchiapas.edu.mx>
2024-03-28 13:14:36 -0600

    Registro Del Usuario

---
+++ src/main/java/org/example/crazycoin/entities/User.java
@@ -0,0 +1,19 @@
+package org.example.crazycoin.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String email;
+    private String password;
+}
