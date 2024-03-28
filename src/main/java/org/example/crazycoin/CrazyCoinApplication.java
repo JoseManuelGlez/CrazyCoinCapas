977a19ffb7995b6ca4780d6a106a443234c3ee9d
José Manuel Galindo González <213505@ids.upchiapas.edu.mx>
2024-03-27 19:20:44 -0600

    first commit

---
+++ src/main/java/org/example/crazycoin/CrazyCoinApplication.java
@@ -0,0 +1,13 @@
+package org.example.crazycoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyCoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crazycoin/CrazyCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crazycoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
