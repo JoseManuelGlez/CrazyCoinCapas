693df35d7c92816c67c7de68059ef115b79dcb7f
José Manuel Galindo González <213505@ids.upchiapas.edu.mx>
2024-03-28 18:48:47 -0600

    first commit

--- src/main/java/org/example/crazycoin/repositories/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.crazycoin.repositories;
-
-import org.example.crazycoin.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepository extends JpaRepository<User, String> {
-}
--- src/main/java/org/example/crazycoin/services/interfaces/IUserService.java
+++
@@ -1,8 +0,0 @@
-package org.example.crazycoin.services.interfaces;
-
-import org.example.crazycoin.controllers.dtos.requests.CreateUserRequest;
-import org.example.crazycoin.controllers.dtos.responses.BaseResponse;
-
-public interface IUserService {
-    BaseResponse create(CreateUserRequest request);
-}
